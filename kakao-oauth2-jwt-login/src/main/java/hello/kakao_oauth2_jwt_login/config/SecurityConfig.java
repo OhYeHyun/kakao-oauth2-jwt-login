b9d85a3fb82cd19560cdc13ffecc9371516dae77
OhYeHyun <ohyhohyl@gmail.com>
2025-05-17 00:57:19 +0900

    feat: [OAuth2 클라이언트 세션] OAuth2UserService 구현

--- kakao-oauth2-jwt-login/src/main/java/hello/kakao_oauth2_jwt_login/config/SecurityConfig.java
+++ kakao-oauth2-jwt-login/src/main/java/hello/kakao_oauth2_jwt_login/config/SecurityConfig.java
@@ -3,36 +3,20 @@ package hello.kakao_oauth2_jwt_login.config;
 import hello.kakao_oauth2_jwt_login.jwt.JwtFilter;
 import hello.kakao_oauth2_jwt_login.jwt.JwtUtil;
 import hello.kakao_oauth2_jwt_login.jwt.LoginFilter;
+import hello.kakao_oauth2_jwt_login.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final AuthenticationConfiguration authenticationConfiguration;
-    private final JwtUtil jwtUtil;
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -47,7 +31,9 @@ public class SecurityConfig {
                 .httpBasic((auth) -> auth.disable());
 
         http
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) ->
+                                userInfoEndpointConfig.userService(customOAuth2UserService)));
 
         http
                 .authorizeHttpRequests((auth) -> auth
@@ -57,12 +43,6 @@ public class SecurityConfig {
                 );
 
         http
-                .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
-
-        http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
-
-        http
                 .formLogin((auth) -> auth.loginPage("/login")
                         .loginProcessingUrl("/loginProc")
                         .permitAll()
@@ -70,10 +50,6 @@ public class SecurityConfig {
 
         http
                 .oauth2Login((auth) -> auth.loginPage("/login"));
-
-        http
-                .sessionManagement((session) ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
---
+++ kakao-oauth2-jwt-login/src/main/java/hello/kakao_oauth2_jwt_login/dto/OAuth2Response.java
@@ -0,0 +1,10 @@
+package hello.kakao_oauth2_jwt_login.dto;
+
+public interface OAuth2Response {
+
+    String getProvider();
+
+    String getProviderId();
+
+    String getNickname ();
+}
