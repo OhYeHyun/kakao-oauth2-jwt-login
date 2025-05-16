0bccd5e870188193db65ebd8178fabdb3e4d8d25
OhYeHyun <ohyhohyl@gmail.com>
2025-05-16 15:53:30 +0900

    프로젝트 생성

---
+++ kakao-oauth2-jwt-login/src/main/java/hello/kakao_oauth2_jwt_login/KakaoOauth2JwtLoginApplication.java
@@ -0,0 +1,13 @@
+package hello.kakao_oauth2_jwt_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoOauth2JwtLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoOauth2JwtLoginApplication.class, args);
+	}
+
+}
---
+++ kakao-oauth2-jwt-login/src/test/java/hello/kakao_oauth2_jwt_login/KakaoOauth2JwtLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.kakao_oauth2_jwt_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoOauth2JwtLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
