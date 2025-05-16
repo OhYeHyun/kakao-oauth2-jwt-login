c071f76284bb9e0cb2d803e88878d04fbb9a3981
OhYeHyun <ohyhohyl@gmail.com>
2025-05-16 22:26:36 +0900

    feat: [JWT] JWT 발급 / 검증 클래스

---
+++ kakao-oauth2-jwt-login/src/main/java/hello/kakao_oauth2_jwt_login/jwt/JwtUtil.java
@@ -0,0 +1,47 @@
+package hello.kakao_oauth2_jwt_login.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private SecretKey secretKey;
+
+    public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createJwt(String username, String role, Long expiredMs) {
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
+
+/**
+ * application.properties 에 지정한 ${spring.jwt.secret} 값으로 secretKey 를 생성
+ * Jwts.parser().verifyWith(secretKey) : 토큰이 우리 서버에서 생성되었는지, 우리 서버에서 생성된 키가 가진 키와 맞는지 확인
+ */
