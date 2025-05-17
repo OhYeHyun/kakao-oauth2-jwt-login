7fe0f882c3ab6e96980d97314f748f9f5b3757cf
OhYeHyun <ohyhohyl@gmail.com>
2025-05-17 16:10:47 +0900

    feat: [OAuth2 클라이언트 세션] main, my 페이지에서 로그인 사용자 nickname 표시

--- kakao-oauth2-jwt-login/src/main/java/hello/kakao_oauth2_jwt_login/controller/MyController.java
+++ kakao-oauth2-jwt-login/src/main/java/hello/kakao_oauth2_jwt_login/controller/MyController.java
@@ -1,13 +1,29 @@
 package hello.kakao_oauth2_jwt_login.controller;
 
+import hello.kakao_oauth2_jwt_login.dto.CustomOAuth2User;
+import hello.kakao_oauth2_jwt_login.dto.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyController {
 
     @GetMapping("/my")
-    public String myP() {
+    public String myP(@AuthenticationPrincipal Object principal, Model model) {
+        System.out.println("Authentication principal = " + principal);
+        System.out.println("Principal class = " + (principal != null ? principal.getClass() : "null"));
+
+        if (principal instanceof CustomUserDetails) {
+            String nickname = ((CustomUserDetails) principal).getNickname();
+            model.addAttribute("nickname", nickname);
+        }
+
+        if (principal instanceof CustomOAuth2User) {
+            String nickname = ((CustomOAuth2User) principal).getName();
+            model.addAttribute("nickname", nickname);
+        }
         return "my";
     }
 }
